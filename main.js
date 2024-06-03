60f2044c98015718a000c8b7e3164480bc08b6a7
Abdelrahman Khaled <abodzoro10@gmail.com>
2024-06-03 21:52:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// const FirstRadio = document.getElementById("generalEnquiry");
-// const SecondRadio = document.getElementById("supportRequest");
-// const FirstBorder = document.getElementById("general-enqiery");
-// const SecondBorder = document.getElementById("support-request");
-// FirstRadio.addEventListener("change", function () {
-//   if (this.checked) {
-//     FirstBorder.style.borderColor = "hsl(169, 82%, 27%)";
-//   } else if (!this.checked) {
-//     container.style.borderColor = "black";
-//   }
-//   {
-//     // Radio is unchecked, reset border color (optional)
-//     container.style.borderColor = "black";
-//   }
-// });
-// SecondRadio.addEventListener("change", function () {
-//   if (this.checked) {
-//     SecondBorder.style.borderColor = "hsl(169, 82%, 27%)";
-//   } else if (!this.checked) {
-//     // Radio is unchecked, reset border color (optional)
-//     this.style.borderColor = "black";
-//   }
-// });
-
 const radioItems = document.querySelectorAll(".radio-item");
 
 radioItems.forEach((item) => {
